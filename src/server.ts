4ea61e88269029aa7fcc2b87bd8fce2ca707d035
Vitor Barbosa <vitorbopelt@gmail.com>
2026-03-10 11:22:38 -0300

    start server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, {urlencoded} from "express";
+import helmet from "helmet";
+import cors from 'cors';
+import { mainRouter } from "./routers/mainRouter.js";
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(urlencoded({ extended: true }));
+server.disable('x-powered-by');
+server.use(express.json());
+
+server.use(mainRouter)
+
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`O server está rodando em http://localhost:${port}`);
+})
+
+
