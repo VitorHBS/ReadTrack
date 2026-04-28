5914ab4e04a9444fa013096d1932961691820aa6
Vitor Barbosa <vitorbopelt@gmail.com>
2026-04-28 15:37:20 -0300

    add function deleteBook

--- public/script.js
+++ public/script.js
@@ -74,17 +74,30 @@ async function carregarLivros() {
     result.data.forEach(book => {
         const item = document.createElement("li");
         item.innerHTML = `
-          <div>
-            <h3>${book.title}</h3>
-            <p>Autor: ${book.author}</p>
-            <p>Páginas: ${book.pages}</p>
-            <p>Status: ${book.status}</p>
-            <p>Nota: ${book.rating ?? "Sem nota"}</p>
-        </div>
-        `;
+          <div class="book-item">
+            <div>
+                <h3>${book.title}</h3>
+                <p>Autor: ${book.author}</p>
+                <p>Páginas: ${book.pages}</p>
+                <p>Status: ${book.status}</p>
+                <p>Nota: ${book.rating ?? "Sem nota"}</p>
+            </div>
+            <div class="book-actions">
+                <button class="btn-edit" data-id="${book.id}">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Z"/>
+                    </svg>
+                    Editar
+                </button>
+                <button class="btn-delete" data-id="${book.id}">
+                    <svg class="icon "xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                    Deletar
+                </button>
+            </div>
+         </div>`;
         lista.appendChild(item);
-
-
     });
 
     document.getElementById("page-info").textContent =
@@ -188,6 +201,34 @@ async function addBook() {
 
         carregarLivros()
         showNotification("Livro adicionado com sucesso", "success")
+
+    } catch (err) {
+        showNotification("Erro de conexão!", "error")
+    }
+}
+
+async function deleteBook(id) {
+
+    try {
+        const response = await fetch(`/book/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                logout()
+            }
+            console.log(response.status)
+            showNotification("Error ao deletar livro", "error")
+            return
+        }
+
+        carregarLivros()
+        showNotification("Livro deletado com sucesso", "success")
 
     } catch (err) {
         showNotification("Erro de conexão!", "error")
@@ -337,6 +378,14 @@ btnRegister.addEventListener("click", async () => {
 })
 
 navLogout.addEventListener("click", logout);
+
+
+lista.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-delete")) {
+        const bookId = e.target.getAttribute("data-id");
+        deleteBook(bookId)
+    }
+})
 
 
 // ------------------- INIT ----------------------------
